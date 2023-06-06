eab838857a7c432d7aa5e42918cf675f63167a50
Prathmesh <pratmbr@gmail.com>
2023-06-06 22:27:45 +0530

    Update UserLogin.jsx

--- src/pages/Auth/UserLogin.jsx
+++ src/pages/Auth/UserLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import "./UserLogin.css";
